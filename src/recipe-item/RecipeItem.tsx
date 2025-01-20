d6a043b2977ab7c718c7728dc119c9d0fb28e14d
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-20 15:46:38 +0300

    Checking for an added recipe

--- src/recipe-item/RecipeItem.tsx
+++ src/recipe-item/RecipeItem.tsx
@@ -7,14 +7,14 @@ const RecipeItem = ({ recipe }) => {
 
 	const dispatch = useDispatch()
 
-	console.log(favorites)
+	const isExists = favorites.some(r => r.id === recipe.id)
 
 	return (
 		<div className={styles.item}>
 			{/* <img src="" alt="" /> */}
 			<h3>{recipe.name}</h3>
 			<button onClick={() => dispatch(actions.toggleFavorites(recipe))}>
-				Add to favorites
+				{isExists ? 'Remove from' : 'Add to'} favorites'
 			</button>
 		</div>
 	)
