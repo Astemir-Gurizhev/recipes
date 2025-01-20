0c483729a1c0e39516d80fc5d68d89632c75fe9e
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-20 15:03:23 +0300

    Initialization of the Redux Toolkit. Create a Recipe Item. Writing the first slice favorite.slice.ts.

---
+++ README.md
@@ -0,0 +1,7 @@
+https://redux-toolkit.js.org/introduction/getting-started
+
+<h1> Redux toolkit </h1>
+
+npm install @reduxjs/toolkit
+
+npm install react-redux
---
+++ src/recipe-item/RecipeItem.tsx
@@ -0,0 +1,11 @@
+import styles from "./RecipeItem.module.css"
+const RecipeItem = ({recipe}) => {
+	return (
+		<div className={styles.item}>
+			{/* <img src="" alt="" /> */}
+			<h3>{recipe.name}</h3>
+			<button>Add to favorites</button>
+		</div>
+	)
+}
+export default RecipeItem
