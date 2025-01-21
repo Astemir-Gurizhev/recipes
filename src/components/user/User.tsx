660f82607dd7ddc12b9fa905323bb143423d4683
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-21 18:40:14 +0300

    checking the user's status

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -1,5 +1,15 @@
-export default function User () {
-	return <div>
-		<h1>User:</h1>
-	</div>
+import { useSelector } from 'react-redux'
+
+export default function User() {
+	const { isLoading, error, user } = useSelector(state => state.user)
+
+	return (
+		<div>
+			{isLoading} ? <div>Loading...</div> : error ?{' '}
+			<div>
+				{error.message} : user ? <h1>User: {user.name}</h1> :{' '}
+				<h1>USER NOT FOUND</h1>{' '}
+			</div>
+		</div>
+	)
 }
