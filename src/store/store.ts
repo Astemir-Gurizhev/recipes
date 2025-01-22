4fd0eb2256b4a5bbb752ebcddb0893f2c2ce1ef0
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-22 13:46:42 +0300

    middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,21 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { createLogger } from 'redux-logger'
+import { api } from './api/api'
 import { reducer as favoritesReducer } from './favorites/favorite.slice'
 import { userSlice } from './user/user.slice'
-import { api } from './api/api'
+
+const logger = createLogger({
+	collapsed: true
+})
 
 const reducers = combineReducers({
 	favorites: favoritesReducer,
 	user: userSlice.reducer,
-	[api.reducerPath]: api.reducer
+	[api.reducerPath]: api.reducer,
 })
 
 export const store = configureStore({
 	reducer: reducers,
-	middleware: (getDefaultMiddleware) => 
-		getDefaultMiddleware().concat(api.middleware),
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware().concat(api.middleware).concat(logger),
 })
